1ce048f9bef08fb731bbf7aa694351d227c2a7f4
Falliot00 <ferminalliot@gmail.com>
2023-04-11 04:31:36 -0300

    BackEnd 1.3
    Añado el componente Educación

---
+++ src/main/java/com/portfolio/Falliot/Repository/REducacion.java
@@ -0,0 +1,17 @@
+
+package com.portfolio.Falliot.Repository;
+
+import com.portfolio.Falliot.Entity.Educacion;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author Fermin
+ */
+@Repository
+public interface REducacion extends JpaRepository<Educacion, Integer>{
+    public Optional<Educacion> findByNombreE(String nombreE);
+    public boolean existsByNombreE(String nombreE);
+}
