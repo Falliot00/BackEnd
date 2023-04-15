56ad3a6bc7545c0cdab8ece0ea22c99647a5e527
Falliot00 <ferminalliot@gmail.com>
2023-04-15 07:11:44 -0300

    BackEnd 1.7.1
    Corrección (Elimino barra de los links)

--- src/main/java/com/portfolio/Falliot/Controller/CEducacion.java
+++ src/main/java/com/portfolio/Falliot/Controller/CEducacion.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = {"https://frontendfalliot.web.app/", "http://localhost:4200"})
+@CrossOrigin(origins = {"https://frontendfalliot.web.app", "http://localhost:4200"})
 public class CEducacion {
     @Autowired
     SEducacion sEducacion;
